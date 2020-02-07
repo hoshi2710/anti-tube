ebf71b7701f712eb4062ba2e41f85cb0ee3f3483
ほし <43630612+vanillaICS@users.noreply.github.com>
2020-02-07 11:58:35 +0900

    third
    팝업창 해상도, 메니페스트 파일 설정

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-$(document).ready( function () {
-    
-
-
-});
 function sw_main(sw_id)
 {
     if($("#"+sw_id+".sw_toggle").css("backgroundColor") == 'rgb(255, 0, 0)')
@@ -36,4 +31,23 @@ function sw(sw_id)
         $("#"+sw_id+" .toggle_dot").css("transform","translate(33px,2px)");
     }
 }
-
+    $("#main_sw").click(function (){
+        sw_main('main_sw');
+    });    
+    $("#hide_comment_sw").click(function (){
+        sw('hide_comment_sw');
+    });
+    $("#recommend_hide_sw").click(function (){
+        sw('recommend_hide_sw');
+    });
+    $("#end_recommend_hide_sw").click(function (){
+        sw('end_recommend_hide_sw');
+    });
+    $("#hide_menu_sw").click(function (){
+        sw('hide_menu_sw');
+    });
+    $("#play_limit_sw").click(function (){
+        sw('play_limit_sw');
+    });
+    
+  
