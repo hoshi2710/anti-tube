3868f3c75ebb9dae828640fc4571518dd7a7cb0f
ほし <43630612+vanillaICS@users.noreply.github.com>
2020-02-07 12:33:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
+var main_sw;
+$(document).ready(function () {
+    chrome.storage.sync.get('main', function(d) {
+        main_sw = d.main;
+        if(main_sw == "on")
+        {
+            $(".sw *").css("transition","0s");
+            $("#main_sw.sw_toggle").css("background-color","red");
+            $(".sw_container").css("background-color","#121f36");
+            $("#main_sw .toggle_dot").css("transform","translate(43px,2px)");
+            $(".sw_status").text("ON");
+            $(".block").fadeOut(200);
+            chrome.storage.sync.set({main : "on"});
+        }
+    });
+
+
+});
+
 function sw_main(sw_id)
 {
     if($("#"+sw_id+".sw_toggle").css("backgroundColor") == 'rgb(255, 0, 0)')
     {
+        $(".sw *").css("transition","0.2s");
         $("#"+sw_id+".sw_toggle").css("background-color","gray");
         $(".sw_container").css("background-color","red");
         $("#"+sw_id+" .toggle_dot").css("transform","translate(2px,2px)");
         $(".sw_status").text("OFF");
         $(".block").fadeIn(200);
+
+        chrome.storage.sync.set({main : "off"});
     }
     else
     {
+        $(".sw *").css("transition","0.2s");
         $("#"+sw_id+".sw_toggle").css("background-color","red");
         $(".sw_container").css("background-color","#121f36");
         $("#"+sw_id+" .toggle_dot").css("transform","translate(43px,2px)");
         $(".sw_status").text("ON");
         $(".block").fadeOut(200);
+        chrome.storage.sync.set({main : "on"});
     }
 }
 
